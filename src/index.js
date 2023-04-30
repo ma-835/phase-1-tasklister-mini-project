2af91d2aaeee4975418e1a7296e238e82f099551
Mahmoud Abdelkarim <mabdelkarim835@gmail.com>
2023-04-30 12:21:59 -0400

    displays task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+addingEventListeners()
 });
+
+
+
+function addingEventListeners(){
+document.getElementById("create-task-form").addEventListener("submit",handleformsubmit)
+}
+
+
+function handleformsubmit(e){
+  e.preventDefault()
+  const task = e.target[0].value
+  displayTask(task)
+}
+
+function displayTask(task){
+const taskUl = document.getElementById("tasks")
+const taskLi = document.createElement("li")
+taskLi.textContent = task 
+taskUl.appendChild(taskLi)
+}
+//stage 1
+// get form element and attach event listener to form
+// get task from input 
+// display data 
